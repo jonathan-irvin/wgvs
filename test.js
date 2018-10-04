031550aaf4325154bdd159efb432c64b24c14a67
Jonathan Irvin <jonathan.irvin@uicentric.com>
2018-10-04 16:17:56 -0500

    feat(Mocha): mocha init with smoke test

---
+++ test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Mocha', function() {
+  describe('smoke test', function() {
+    it('should be true if true', function() {
+      assert.equal(true,true);
+    });
+  });
+});
